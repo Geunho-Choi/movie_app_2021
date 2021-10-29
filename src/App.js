85e3e4cafeaaf82f5ec995e681568d493cd8ad9d
Geunho Choi <ckh2156@gmail.com>
2021-10-29 17:01:44 +0900

    update react-lecture 3.1

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ fav }) {
-  return <h3>I love {fav}</h3>;
-}
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
+  minus = () => {
+    console.log("minus");
+    this.setState((current) => ({
+      count: current.count - 1,
+    }));
+  };
 
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-  },
-  {
-    name: "bulgogi",
-    image:
-      "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
-  },
-  {
-    name: "kimbap",
-    image:
-      "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
-  },
-  {
-    name: "samgyetang",
-    image:
-      "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-  },
-];
-
-function App() {
-  return (
-    <div>
-      Hello
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgupsal" />
-      <Food fav="jjukumi" />
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
