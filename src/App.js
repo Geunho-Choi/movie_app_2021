22ce3d188e921061c7bb0528b229334621e53d20
Geunho Choi <ckh2156@gmail.com>
2021-11-01 14:45:02 +0900

    update react-lecture 4.1 rendering movies(Movie function component)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -8,7 +9,15 @@ class App extends React.Component {
   };
   getMovies = async () => {
     // axios : 땅콩 주위의 초콜릿.
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+    // 4.1 api로 가져온 데이터 살펴보기
   };
   // 4.0 axios.get이 완료되기까지 시간이 조금 필요하기 때문에 await을 넣어줌.
   // await과 async는 함께 쓰임.
@@ -19,8 +28,23 @@ class App extends React.Component {
   // 3.2 React Component에서 사용하는 유일한 함수는 render함수.
   render() {
     // 3.3 ES6 문법
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 // 처음에 우리가 render를 하면 호출되는 life cycle method는 뭘까?-->componentDidMount
