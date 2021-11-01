506625e21d9095083daa68f859e7442111510b87
Geunho Choi <ckh2156@gmail.com>
2021-11-01 11:01:55 +0900

    update react-lecture 4.0 fetch movies from api(axios)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movie: [],
   };
+  getMovies = async () => {
+    // axios : 땅콩 주위의 초콜릿.
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  };
+  // 4.0 axios.get이 완료되기까지 시간이 조금 필요하기 때문에 await을 넣어줌.
+  // await과 async는 함께 쓰임.
   componentDidMount() {
-    // timeout은 react것이 아니라 JS꺼임.
-    // 6초 후에 발생.
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
+
   // 3.2 React Component에서 사용하는 유일한 함수는 render함수.
   render() {
     // 3.3 ES6 문법
