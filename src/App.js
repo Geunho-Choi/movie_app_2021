9fa5c822b4c9226f4c8b841b5ef2aee5165eb4f2
Geunho Choi <ckh2156@gmail.com>
2021-11-01 09:58:40 +0900

    update react-lecture 3.2 component life cycle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0,
   };
   add = () => {
-    console.log("add");
+    // console.log("add");
     this.setState((current) => ({
       count: current.count + 1,
     }));
   };
   minus = () => {
-    console.log("minus");
+    // console.log("minus");
     this.setState((current) => ({
       count: current.count - 1,
     }));
   };
+  // componentDidMount: component가 처음 render될 때 호출됨
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  // componentDidUpdate: component가 업데이트될 때 호출됨
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
 
+  // 3.2 React Component에서 사용하는 유일한 함수는 render함수.
   render() {
+    console.log("I am rendering");
     return (
       <div>
         <h1>The number is {this.state.count}</h1>
